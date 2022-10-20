c227f9350a2c82ea9f703eaddcaffb25b5253b9a
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2022-10-20 13:59:30 -0500

    Read a file

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+let fs = require('fs');
+
+fs.readFile('archivo.txt', 'utf-8', (err, data) => {
+  if(err) {
+    console.log('error: ', err);
+  } else {
+    console.log(data);
+  }
+});
