c91252a79c52fb62c16e6acab2c67f6ae8b94d8d
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2022-10-25 12:01:41 -0500

    From absolute to relative 2°

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const fs = require('fs');
 const path = require('path');
-//const {resolve} = require('path');
+const {resolve} = require('path');
 
-let route = 'archivo.txt'
-// if (route == absolutePath){
-//   console.log('The path was absolute!!!');
-// }else{
-//   console.log(absolutePath);
-// }
-const isAbsolutePath =  path.isAbsolute(route);
-console.log(isAbsolutePath);
+let route = 'archivo.txt';
 
+//let route = 'C:\Users\Zarakem\Documents\A_Laboratoria\Proyecto 4- Markdown Links\MarkdownLinks\CDMX013-md-links-AGZ\archivo.txt';
 
+let absolutePath = path.isAbsolute(route) // true is absolute
+if (absolutePath === false) {
+ route = path.resolve(route);
+ console.log(route);
+}else{
+ console.log('The path was absolute!!!');
+}
 
-// if (fs.existsSync(path)) {
-//   console.log('The file is here! and it says:');
-// } else {
-//   console.log('Nothing here!');
-// }
+if (fs.existsSync(route)) {
+console.log('The file is here! and it says:');
+} else {
+  console.log('Nothing here!');
+}
 
-// fs.readFile(path, 'utf-8', (err, data) => {
-//   if(err) {
-//     console.log('But, why?:', err);
-//   } else {
-//     console.log(data);
-//   }
-// });
+fs.readFile(route, 'utf-8', (err, data) => {
+  if(err) {
+    console.log('But, why?:', err);
+  } else {
+    console.log(data);
+  }
+});
