784f0781ece29ad60aaf213bc077d351f61b6c44
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2022-10-24 20:08:30 -0500

    Reading and validating a file

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-// module.exports = () => {
-//   // ...
-// };
-let fs = require('fs');
+const fs = require('fs');
+const path = require('path');
+//const {resolve} = require('path');
 
-fs.readFile('archivo.txt', 'utf-8', (err, data) => {
-  if(err) {
-    console.log('error: ', err);
-  } else {
-    console.log(data);
-  }
-});
+let route = 'archivo.txt'
+// if (route == absolutePath){
+//   console.log('The path was absolute!!!');
+// }else{
+//   console.log(absolutePath);
+// }
+const isAbsolutePath =  path.isAbsolute(route);
+console.log(isAbsolutePath);
+
+
+
+// if (fs.existsSync(path)) {
+//   console.log('The file is here! and it says:');
+// } else {
+//   console.log('Nothing here!');
+// }
+
+// fs.readFile(path, 'utf-8', (err, data) => {
+//   if(err) {
+//     console.log('But, why?:', err);
+//   } else {
+//     console.log(data);
+//   }
+// });
