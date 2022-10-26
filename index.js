aa40862d7ab5b82f585ec90a807f4d496c5448f8
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2022-10-26 00:45:32 -0500

    Separate functions of the path in another file.

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-const colors = require('colors');
-const fs = require('fs');
-const path = require('path');
-const {resolve} = require('path');
+const fn = require('./path');
 
-let route = 'archivo.txt';
-
-//let route = 'C:\Users\Zarakem\Documents\A_Laboratoria\Proyecto 4- Markdown Links\MarkdownLinks\CDMX013-md-links-AGZ\archivo.txt';
-
-let absolutePath = path.isAbsolute(route) // true is absolute
-
-if (absolutePath === false) { // Checks if the path is absolute
- route = path.resolve(route);
- console.log(route.green);
-}else{
- console.log('The path was absolute!!!');
-}
-
-if (fs.existsSync(route)) { // 
-console.log('The file is here! and it says:'.yellow);
-} else {
-  console.log('Nothing here!'.magenta);
-}
-
-fs.readFile(route, 'utf-8', (err, data) => {
-  if(err) {
-    console.log('But, why?:'.red, err);
-  } else {
-    console.log(data.blue);
-  }
-});
+console.log(fn.isPathAbsolute(),fn.isThereAFile(), fn.readTheFile());
---
+++ path.js
@@ -0,0 +1,36 @@
+const colors = require('colors');
+const fs = require('fs');
+const path = require('path');
+const {resolve} = require('path');
+let route = 'archivo.txt';
+
+ function isPathAbsolute ()  {
+    let absolutePath = path.isAbsolute(route) // true is absolute
+    if (absolutePath === false) { // Checks if the path is absolute
+     route = path.resolve(route);
+     console.log(route.green);
+    }else{
+     console.log('The path was absolute!!!');
+    }
+};
+
+function isThereAFile (){
+    if (fs.existsSync(route)) { // 
+        console.log('The file is here! and it says:'.yellow);
+        } else {
+          console.log('Nothing here!'.magenta);
+        }
+};
+function readTheFile (){
+        fs.readFile(route, 'utf-8', (err, data) => {
+          if(err) {
+            console.log('But, why?:'.red, err);
+          } else {
+            console.log(data.blue);
+          }
+        })
+    };
+
+module.exports = {
+    isPathAbsolute, isThereAFile, readTheFile
+}
